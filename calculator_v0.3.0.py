6ba0aa4258b9b23072ada16fa6d289b71e0cfe9a
aliattack1 <83272842+aliattack1@users.noreply.github.com>
2023-12-03 22:32:35 +0330

    v 0.3.0
    adding minus operator and re structuring the operation adding

--- calculator_v0.3.0.py
+++ calculator_v0.3.0.py
@@ -2,6 +2,11 @@ class calculator:
 	
 	
 	
+	def plus(a,b):
+		return int(a)+int(b)
+	
+	
+	operation_method_dict = {'+': plus}
 	operations_string = '+'
 	
 	@classmethod
@@ -22,16 +27,17 @@ class calculator:
 		for s in used_operation_index_list:
 		    lis.append(inp[s])
 		    if len(used_operation_index_list) > new_count + 1:
-		    	lis.append(inp[s:used_operation_index_list[new_count+1]])
+		    	lis.append(inp[s+1:used_operation_index_list[new_count+1]])
 		    else:
 		    	lis.append(inp[s+1:])
 		    new_count += 1
 		return lis
 	
-	@staticmethod
-	def callop(num1, operation, num2):
-		if operation == '+':
-			return int(num1) + int(num2)
+	@classmethod
+	def callop(cls, num1, operation, num2):
+		if operation in cls.operations_string:
+			return cls.operation_method_dict[operation](num1,num2)
+			
 	
 	
 	@classmethod
@@ -49,5 +55,15 @@ class calculator:
 	def calculate(cls, inp):
 		return(cls.operations(cls.input_to_list(inp)))
 	
+	@classmethod
+	def operation_register(cls, opname, func):
+		cls.operation_method_dict[opname] = func
+		cls.operations_string += opname
 
-print(calculator.calculate(input()))
+def minus(num1, num2):
+	return int(num1) - int(num2)
+
+
+calculator.operation_register('-', minus)
+
+print('    ', calculator.calculate(input('     ')))
