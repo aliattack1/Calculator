50cd272a2df85e17a02f5993822fb6372d876922
aliattack1 <83272842+aliattack1@users.noreply.github.com>
2023-12-09 15:19:42 +0330

    completing update for v 0.7.0

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from . import plus, minus, multiple, divide, power
+dictionary = {'+': plus, '-': minus, '*': multiple, '/': divide, '^': power}
+importance_dictionary = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
