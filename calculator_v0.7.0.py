80f778cabf08f650543884b484a7e975f30f6840
aliattack1 <83272842+aliattack1@users.noreply.github.com>
2023-12-09 15:18:37 +0330

    v 0.7.0
    adding some structures to calculate in mathematical order (its having bugs for parantheses

--- calculator_v0.7.0.py
+++ calculator_v0.7.0.py
@@ -7,6 +7,8 @@ class Calculator:
     operations_string = ''
     negetive_number = False
     negetive_number_right = None
+    operations_importance = ""
+    importance_dictionary = {}
 
     @classmethod
     def parantese(cls, inp):
@@ -84,6 +86,71 @@ class Calculator:
             return cls.operations(cls.input_to_list(inp))
 
     @classmethod
+    def importance_list(cls, inp):
+        ilist = {}
+        n = 0
+        for i in inp:
+            if i in cls.operations_string:
+                ilist[n] = (op.importance_dictionary[i])
+            n += 1
+        return ilist
+
+
+    @classmethod
+    def index_finder(cls, lis, place):
+        count = -1
+        index = 0
+        for i in lis:
+            count += len(i)
+            if count == place:
+                return index
+            index += 1
+
+
+    @staticmethod
+    def gstr(lis):
+        l = ""
+        for a in lis:
+            l += a
+        return l
+
+    @classmethod
+    def order_call(cls, inp, place):
+        lis = cls.input_to_list(inp)
+        index = cls.index_finder(lis, place)
+        m = lis[index-1:index+2]
+        l = cls.gstr(m)
+        answer = cls.calculate(l)
+        return cls.gstr(lis[:index-1]) + str(answer) + cls.gstr(lis[index+2:])
+
+
+
+    @classmethod
+    def order(cls, inp):
+        imlist = cls.importance_list(inp)
+        ilist = list(imlist.keys())
+        nlist = []
+        for i in ilist:
+            nlist.append(imlist[i])
+
+        while max(nlist) > 1:
+            s = max(nlist)
+            f = ""
+            for a in ilist:
+                f += str(imlist[a])
+            inp = cls.order_call(inp, ilist[f.index(str(s))])
+            imlist = cls.importance_list(inp)
+
+            ilist = list(imlist.keys())
+            nlist = []
+            for i in ilist:
+                nlist.append(imlist[i])
+            if nlist == []:
+                return inp
+        return cls.calculate(inp)
+
+
+    @classmethod
     def operation_register(cls, opname, func):
         cls.operation_method_dict[opname] = func
         cls.operations_string += opname
@@ -91,6 +158,6 @@ class Calculator:
 
 for key in op.dictionary:
     Calculator.operation_register(key, op.dictionary[key].action)
+Calculator.importance_dict = op.importance_dictionary
 
-
-print('    ', Calculator.calculate(input('     ')))
+print(Calculator.order(input()))
