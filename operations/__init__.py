b965ac70face434aa352bbbf98b0e8fe3c056434
aliattack1 <83272842+aliattack1@users.noreply.github.com>
2023-12-16 17:48:31 +0330

    added sinus package for next update!

--- operations/__init__.py
+++ operations/__init__.py
@@ -1,2 +1,3 @@
-from . import plus, minus, multiple, divide, power
-dictionary = {'+': plus, '-': minus, '*': multiple, '/': divide, '^': power}
+from . import plus, minus, multiple, divide, power, sinus
+dictionary = {'+': plus, '-': minus, '*': multiple, '/': divide, '^': power, 'sin': sinus}
+importance_dictionary = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, 'sin': 4}
