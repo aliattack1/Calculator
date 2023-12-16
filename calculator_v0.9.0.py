9385d972665152f1595bbe5b037b4317cf6210f5
aliattack1 <83272842+aliattack1@users.noreply.github.com>
2023-12-16 17:50:45 +0330

    v 0.10.0
    added ability to use functions like sin , cos , ln and etc.....
    added sin

--- calculator_v0.9.0.py
+++ calculator_v0.9.0.py
@@ -4,15 +4,14 @@ import operations as op
 class Calculator:
 
     operation_method_dict = {}
-    operations_string = ''
+    operations_string = '@'
     negative_number = False
     negative_number_right = None
     operations_importance = ""
     importance_dictionary = {}
-
+    function_list = []
     @classmethod
     def input_checker(cls, inp):
-
         parentheses_finished = None
         countains_letter = None
         letters_string = "qwertyuioplkjhgfdsazxcvbnm"
@@ -35,7 +34,8 @@ class Calculator:
         if not parentheses_finished:
             raise Exception("you have a wrong used parentheses fix it and retry")
         if countains_letter:
-            raise Exception("you used a letter in input we dont support variables so you should re try without letter")
+            pass
+            # raise Exception("you used a letter in input we dont support variables so you should re try without letter")
         inp_without_space = ""
         for char in inp:
             if char != " ":
@@ -58,34 +58,67 @@ class Calculator:
                 break
             letter_count += 1
         inp1 = inp[:letter_count+1]
-        print(inp1)
         left_parentheses_index = inp1.find('(')
         right_parenthesess_index = len(inp1) - (inp1[::-1].find(')')) - 1
         return inp[left_parentheses_index + 1:right_parenthesess_index], inp[0:left_parentheses_index], inp[right_parenthesess_index + 1:]
 
     @classmethod
-    def input_to_list(cls, inp):
+    def input_to_list(cls, inp, retmod=0):
         output_list = []
         used_operation_list = []
         used_operation_index_list = []
         operation_list_loop_count = 0
+        used_function_list = []
+        used_function_index_list = []
+
+        functions_finished = False
+        while not functions_finished:
+            found_function = False
+            for i in cls.function_list:
+                if i in inp:
+                    used_function_list.append(i)
+                    inp = inp[:inp.find(i)] + "@" + inp[inp.find(i)+len(i):]
+                    found_function = True
+            if not found_function:
+                functions_finished = True
 
         for letter in inp:
             if letter in cls.operations_string:
-                used_operation_list.append(letter)
-                used_operation_index_list.append(operation_list_loop_count)
-
+                if letter =="@":
+                    used_operation_list.append(used_function_list[0])
+                    used_function_list.pop(0)
+                    used_operation_index_list.append(operation_list_loop_count)
+                else:
+                    used_operation_list.append(letter)
+                    used_operation_index_list.append(operation_list_loop_count)
             operation_list_loop_count += 1
+
+        if retmod == 1:
+            return used_operation_list, used_operation_index_list
+
         output_list.append(inp[0:used_operation_index_list[0]])
         list_filing_loop_count = 0
         for operation_index in used_operation_index_list:
-            output_list.append(inp[operation_index])
+            output_list.append(used_operation_list[list_filing_loop_count])
+
             if len(used_operation_index_list) > list_filing_loop_count + 1:
                 output_list.append(inp[operation_index + 1:used_operation_index_list[list_filing_loop_count + 1]])
             else:
                 output_list.append(inp[operation_index + 1:])
             list_filing_loop_count += 1
-        return output_list
+        if retmod == 0:
+            return output_list
+
+
+    @classmethod
+    def callfu(cls, num1, function, num2):
+        if function in cls.function_list:
+            if cls.negative_number:
+                cls.negative_number = False
+                if cls.negative_number_right:
+                    return float(cls.operation_method_dict[function](float(num1), -float(num2)))
+            else:
+                return float(cls.operation_method_dict[function](float(num1), float(num2)))
 
     @classmethod
     def callop(cls, num1, operation, num2):
@@ -93,21 +126,24 @@ class Calculator:
             if cls.negative_number:
                 cls.negative_number = False
                 if cls.negative_number_right:
-                    return cls.operation_method_dict[operation](int(num1), -int(num2))
+                    return float(cls.operation_method_dict[operation](float(num1), -float(num2)))
                 else:
-                    return cls.operation_method_dict[operation](-int(num1), int(num2))
+                    return float(cls.operation_method_dict[operation](-float(num1), float(num2)))
             else:
-                return cls.operation_method_dict[operation](int(num1), int(num2))
+                return float(cls.operation_method_dict[operation](float(num1), float(num2)))
 
     @classmethod
-    def operations(cls, input_list):
+    def operations_and_functions(cls, input_list):
         operation_count = 0
         last_number = input_list[0]
         for input_part in input_list:
 
             if input_part in cls.operations_string:
                 new_number = input_list[operation_count + 1]
-                last_number = (cls.callop(last_number, input_part, new_number))
+                last_number = cls.callop(last_number, input_part, new_number)
+            elif input_part in cls.function_list:
+                new_number = input_list[operation_count + 1]
+                last_number = cls.callfu(last_number, input_part, new_number)
             operation_count += 1
         return last_number
 
@@ -116,7 +152,7 @@ class Calculator:
         if '(' in inp or ')' in inp:
             inp_part_1, inp_part_2, inp_part_3 = cls.parentheses(inp)
             new_inp = cls.calculate(inp_part_1)
-            if int(new_inp) >= 0:
+            if float(new_inp) >= 0:
                 return cls.calculate(inp_part_2 + str(new_inp) + inp_part_3)
             else:
                 if inp_part_2 != "":
@@ -133,10 +169,10 @@ class Calculator:
     @classmethod
     def input_to_importance_dict(cls, inp):
         importance_dict = {}
+        used_operation_list, used_operation_index_list = cls.input_to_list(inp, 1)
         count = 0
-        for i in inp:
-            if i in cls.operations_string:
-                importance_dict[count] = (op.importance_dictionary[i])
+        for i in used_operation_index_list:
+            importance_dict[i] = cls.importance_dict[used_operation_list[count]]
             count += 1
         return importance_dict
 
@@ -144,7 +180,7 @@ class Calculator:
     @classmethod
     def index_finder(cls, lis, place):
         count = -1
-        index = 0
+        index = 1
         for i in lis:
             count += len(i)
             if count == place:
@@ -162,9 +198,9 @@ class Calculator:
     @classmethod
     def order_call(cls, inp, place):
         lis = cls.input_to_list(inp)
-        index = cls.index_finder(lis, place)
+        index = cls.index_finder(lis, place-1)
         seperated_input_part = cls.get_string(lis[index-1:index+2])
-        answer = cls.operations(cls.input_to_list(seperated_input_part))
+        answer = cls.operations_and_functions(cls.input_to_list(seperated_input_part))
         return cls.get_string(lis[:index - 1]) + str(answer) + cls.get_string(lis[index + 2:])
 
 
@@ -176,7 +212,6 @@ class Calculator:
         importance_list_values = []
         for key in importance_list_keys:
             importance_list_values.append(importance_list[key])
-
         while max(importance_list_values) > 1:
             max_importance_value = max(importance_list_values)
             importance_values_string = ""
@@ -191,17 +226,21 @@ class Calculator:
                 importance_list_values.append(importance_list[key])
             if not importance_list_values:
                 return inp
-        return cls.operations(cls.input_to_list(inp))
+        return cls.operations_and_functions(cls.input_to_list(inp))
 
 
     @classmethod
-    def operation_register(cls, opname, func):
-        cls.operation_method_dict[opname] = func
-        cls.operations_string += opname
+    def operation_and_function_register(cls, opname, func):
+        if len(opname) == 1:
+            cls.operation_method_dict[opname] = func
+            cls.operations_string += opname
+        else:
+            cls.operation_method_dict[opname] = func
+            cls.function_list.append(opname)
 
 
 for key in op.dictionary:
-    Calculator.operation_register(key, op.dictionary[key].action)
+    Calculator.operation_and_function_register(key, op.dictionary[key].action)
 Calculator.importance_dict = op.importance_dictionary
 
 print(Calculator.calculate(Calculator.input_checker(input())))
